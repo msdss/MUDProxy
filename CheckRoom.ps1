f427c06e5e8c3e47742dbf94a1b77ad42cc3070a
msdss <charles.christena@gmail.com>
2026-03-14 16:23:11 -0500

    deleted old files.

--- CheckRoom.ps1
+++
@@ -1,3 +0,0 @@
-$rooms = Get-Content 'C:\Users\MSD\AppData\Roaming\MudProxyViewer\Game Data\Rooms.json' -Raw | ConvertFrom-Json
-$r = $rooms | Where-Object { $_.'Map Number' -eq 10 -and $_.'Room Number' -eq 261 }
-$r | Format-List
